adbd6d1a237c3c5821f63cee2e28f71268af4059
Marco Fiordi <marco.fiordi97@gmail.com>
2026-06-09 16:49:38 +0200

    aggiunto contatore degli eventi

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
-import Main from "./components/Main/Main";
 
+import { events } from "../data/events";
 
 function App() {
   return (
-    <Main />
-  )
+    <main>
+      <h1>EventBoard</h1>
+
+      <p>Eventi trovati: {events.length}</p>
+
+      <ul>
+        {events.map((event) => (
+          <li key={event.id}>
+            <h2>{event.title}</h2>
+            <p>Data: {event.date}</p>
+            <p>Categoria: {event.category}</p>
+            <p>Stato: {event.status}</p>
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
 }
 export default App;
